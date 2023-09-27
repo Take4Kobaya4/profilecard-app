69bd8eca538adbe6fe2ca42279d7456e25f81e73
Takeshi Kobayashi <t.kobayashi3927@gmail.com>
2023-09-27 13:52:51 +0900

    propsの部分を{title, handle}に変更し、constで定義していたものを削除。

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,5 +1,12 @@
-function ProfileCard(){
-  return <div>Profile Card</div>;
+function ProfileCard({title, handle}){
+  // returnする前に定数として定義
+  
+  return (
+    <div>
+      <div>Title is {title}</div>
+      <div>Handle is {handle}</div>
+    </div>
+  );
 }
 
 export default ProfileCard;
